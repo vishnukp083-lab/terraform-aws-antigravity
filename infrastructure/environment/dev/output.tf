875453e23aebea9664b62ad8614615e6d7bafcbc
vishnukp083-lab <vishnukp083@gmail.com>
2025-12-28 14:57:31 -0500

    Update output.tf

--- infrastructure/environment/dev/output.tf
+++ infrastructure/environment/dev/output.tf
@@ -1,10 +1,9 @@
 output "instance_id" {
-    description = "ID of the instance"
-    value       =  aws_instance.app_study_instance.id
-
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_study_instance.id
 }
 
 output "instance_public_ip" {
-    description = "display public ip of the instance"
-    value       = aws_instance.app_study_instance.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app_study_instance.public_ip
 }
