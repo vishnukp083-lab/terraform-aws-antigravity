bc0a525ee8049b3be2cfb158164587a7356896c2
vishnukp083-lab <vishnukp083@gmail.com>
2025-12-28 14:44:03 -0500

    Update output.tf

--- infrastructure/environment/dev/output.tf
+++ infrastructure/environment/dev/output.tf
@@ -7,8 +7,4 @@ output "instance_id" {
 output "instance_public_ip" {
     description = "display public ip of the instance"
     value       = aws_instance.app_study_instance.public_ip
-}
-#output "private_key_pem" {
- # value     = tls_private_key.ec2_key.private_key_pem
-  #sensitive = true
-#}
+
