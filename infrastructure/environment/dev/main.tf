ec227f19682a48b8fb11d5826b61e1e01b3036ab
vishnukp083-lab <vishnukp083@gmail.com>
2025-12-28 13:53:29 -0500

    Update main.tf

--- infrastructure/environment/dev/main.tf
+++ infrastructure/environment/dev/main.tf
@@ -73,13 +73,6 @@ ingress{
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
-}
-egress{
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-}
   ingress {
     from_port   = 22
     to_port     = 22
