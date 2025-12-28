13732f324f6f7860042164eabf622e49fe7404e9
vishnukp083-lab <vishnukp083@gmail.com>
2025-12-28 13:36:51 -0500

    Update main.tf

--- infrastructure/environment/dev/main.tf
+++ infrastructure/environment/dev/main.tf
@@ -76,6 +76,18 @@ egress{
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
 }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 #Create EC2 instance
