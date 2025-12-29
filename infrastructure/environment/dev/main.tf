ed7e3f6f2706bc4a49708c3a15fe7341b6efce55
Vishnu KP <vishnukp083@gmail.com>
2025-12-28 21:33:45 -0500

    main manifest changed

--- infrastructure/environment/dev/main.tf
+++ infrastructure/environment/dev/main.tf
@@ -57,16 +57,24 @@ tags = {
 ## Create new security group
 
 resource "aws_security_group" "app_study_sg"{
-    name = "HTTP"
+    
     vpc_id = aws_vpc.app_study_vpc.id
 
 ingress{
+    description = "HTTP"
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
 }
+ingree{
+    description = "SSH"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
 egress{
     from_port = 0
     to_port   = 0
